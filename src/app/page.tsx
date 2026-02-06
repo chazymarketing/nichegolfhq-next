2878ff784433c88e1386a77fb9a72f6aab9c669a
Jonathan <Jonathan@chazymarketing.com>
2026-02-06 10:18:47 -0600

    Home: rename channel subscribe button to newsletter sign up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default async function Home() {
                     href={`/${feed.slug}#subscribe`}
                     className="inline-flex items-center justify-center rounded-full bg-zinc-950 px-4 py-2 text-sm font-medium text-white hover:bg-zinc-800"
                   >
-                    Subscribe
+                    Newsletter sign up
                   </Link>
 
                   {feed.xProfileUrl && (
